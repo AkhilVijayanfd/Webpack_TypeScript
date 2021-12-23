d45e6b823b2d8af3fc7c23b1c6f79ef4d6e06710
AkhilVijayanfd <92812336+AkhilVijayanfd@users.noreply.github.com>
2021-12-23 17:35:58 +0530

    Partially add Validation
    Partially add validation

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,12 +2,16 @@
 import { BrowserRouter as Router,Switch,Route,Link } from 'react-router-dom';
 import Home from '../Pages/Home';
 import About from '../Pages/About';
-import Dashboard from '../Pages/Dashboard';
 import ProtectedRoute from './ProtectedRoute';
+import Login from '../Pages/Login';
+import { useState } from 'react';
+import Profile from '../Pages/Profile';
 
 
 function Routes () 
 {
+
+    const [isAuth, setIsAuth] = useState(false);
     return (
 
     <Router>
@@ -18,10 +22,10 @@ function Routes ()
                 <Link to="/" >Home</Link>
             </li>
             <li>
-                <Link to="/about">About (Protected)</Link>
+                <Link to="/about">About</Link>
             </li>
             <li>
-                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/login">Login</Link>
             </li>
         </ul>
 
@@ -29,10 +33,11 @@ function Routes ()
             <Route exact path="/">
                 <Home />
             </Route>
-            <Route path="/dashboard">
-                <Dashboard />
-            </Route>
-            <ProtectedRoute path="/about" component={About} auth={false}/>
+            <Route path="/about">
+                <About />
+            </Route> 
+            <Route path="/login" component={() => <Login setIsAuth = {setIsAuth}/>}/>
+           <ProtectedRoute isAuth={isAuth} path="/profile" component={Profile} />
         </Switch>
     </div>
 </Router>
