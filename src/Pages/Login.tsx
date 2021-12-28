bcd6b11f7d0f189449591adfd150c6056e6c4a50
AkhilVijayanfd <92812336+AkhilVijayanfd@users.noreply.github.com>
2021-12-28 10:08:31 +0530

    Update Login.tsx

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,78 +1,57 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import '../styles.css';
 
 interface Props {
     setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+const Value = [
+  {username: "akhil", password:"akhil@123"},
+  {username: "akhilvijayan", password:"akhil@111"}
+]
+
+
 const Login: React.FC<Props> = ({ setIsAuth }) => {
+    
+    const [ username, setUsername ] = useState("");
+    const [ password, setPassword ] = useState("");
     const history = useHistory();
-    const handleLogin = () => {
+    const handleLogin = (username: string, password: string) => {
+
+      
+      for(let i =0; i < Value.length; i++)
+      {
+        if (username == Value[i].username && password == Value[i].password)
+        {
         setIsAuth(true);
         history.push("/profile");
-    };
-    const initialValues = { name: "", password: "" };
-    const [formValues, setFormValues] = useState(initialValues);
-    const [formErrors, setFormErrors] = useState({});
-    const [isSubmit, setIsSubmit] = useState(false);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues({ ...formValues, [name]: value });
-      };
-    
-    const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormErrors(validate(formValues));
-    setIsSubmit(true);
+        }
+      }
     };
 
-    useEffect(() => {
-        console.log(formErrors);
-        if (Object.keys(formErrors).length === 0 && isSubmit) {
-          console.log(formValues);
-        }
-      }, [formErrors]);
-
-      const validate = (values) => {
-        const errors = {};
-        if (!values.username) {
-          errors.username = "Username is required!";
-        }
-
-        if (!values.password) {
-          errors.password = "Password is required";
-        }
-        return errors;
-      };
     return( 
 
-    <form onSubmit={handleSubmit}>
+    <form>
     <h1>Login Form</h1>
     <div className="login">
-    <label>Username :</label>
+    <label>username :</label>
     <input 
     type="text"
     name="username"
     placeholder="Username"
-    value={formValues.name}
-    onChange={handleChange}
     />
-    <p>{formErrors.username}</p>
     <br></br>
     <label>password :</label>
     <input
     type="password"
     name="password"
     placeholder="password"
-    value={formValues.password}
-    onChange={handleChange}
+
     />
-    <p>{formErrors.password}</p>
     <br></br>
-    <button onClick={() => handleLogin()}>Submit</button>
+    <button onSubmit={() => handleLogin() }>Submit</button>
     </div>
     </form>
     );
